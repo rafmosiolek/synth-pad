c2aa87c0d0321fabb6407b3ca10d69d5dbe033bc
RafM <rkjmosiolek@gmail.com>
2017-06-14 20:49:30 +0100

    fix styling remove bootstrap add jquery libraries

--- gulpfile.js
+++ gulpfile.js
@@ -147,7 +147,6 @@ gulp.task('serve:test', ['scripts'], () => {
 gulp.task('wiredep', () => {
   gulp.src('app/*.html')
     .pipe(wiredep({
-      exclude: ['bootstrap.js'],
       ignorePath: /^(\.\.\/)*\.\./
     }))
     .pipe(gulp.dest('app'));
