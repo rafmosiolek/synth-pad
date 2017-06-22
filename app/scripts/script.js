7b1c2ad39bba617b131fa832794ef8de5c49062c
RafM <rkjmosiolek@gmail.com>
2017-06-22 15:58:54 +0100

    add favicon

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,24 +1,46 @@
-  function removeTransition(e) {
-    if (e.propertyName !== 'transform') return;
-    e.target.classList.remove('playing');
-  }
+// declare variables
+var audio, pad, pads;
 
-  function playSound(e) {
-    var audio = document.querySelector('audio[data-key]');
-    var pad = document.querySelector('button[data-key]');
+audio = document.querySelector("audio");
+pad = document.querySelector("button");
 
-    if (!audio) return;
 
-    pad.classList.add('playing');
-    audio.currentTime = 0;
-    audio.play();
-  }
 
-  var pads = Array.from(document.querySelectorAll('#pads'));
-  pads.forEach(function(pad) {
-    return pad.addEventListener('transitionend', removeTransition);
-  });
+window.addEventListener("keydown", playSound);
+pad.addEventListener("click", playSound);
 
-  window.addEventListener('keydown', playSound); 
+
+
+function removeTransition(e) {
+	if (e.propertyName !== 'transform') return;
+	e.target.classList.remove('playing');
+}
+
+function playSound(e) {
+	// var audio = document.querySelector('audio[data-key]');
+	// var pad = document.querySelector('button[data-key]');
+
+
+
+	// var audioData = audio.getAttribute("data-key");
+	// var padData = audio.getAttribute("data-key");
+
+	// if (!audio) return;
+
+
+
+	pad.classList.add('playing');
+	audio.currentTime = 0;
+	audio.play(); 
+
+}
+
+pads = Array.from(document.querySelectorAll('#pads'));
+
+pads.forEach(function(pad) {
+	return pad.addEventListener('transitionend', removeTransition);
+});
+
+// window.addEventListener('keydown', playSound); 
 
 
