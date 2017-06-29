9a1b30cb8d0e7ea3241e960fffc1f7168ffcf4ba
RafM <rkjmosiolek@gmail.com>
2017-06-29 11:24:51 +0100

    fix playSound(e)

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,15 +1,21 @@
 // declare variables
-var audio, pad, pads;
+var audio, pad, pads, audioData;
 
-audio = document.querySelector("audio");
-pad = document.querySelector("button");
+audio = document.querySelectorAll("audio");
+pad = document.querySelectorAll("button");
+audioData = document.querySelectorAll("audio[src]");
 
-
-
-window.addEventListener("keydown", playSound);
-pad.addEventListener("click", playSound);
+var padAudio = document.querySelectorAll("button[data-sound='sounds/kick.wav']");
 
 
+// window.addEventListener("keydown", playSound);
+// pad.addEventListener("click", playSound);
+
+for (var i = 0; i < pad.length; i++) {
+
+	pad[i].addEventListener("click", playSound);
+
+}
 
 function removeTransition(e) {
 	if (e.propertyName !== 'transform') return;
@@ -28,12 +34,35 @@ function playSound(e) {
 	// if (!audio) return;
 
 
+		e.currentTarget.classList.add('playing');
+		var soundURL = e.currentTarget.getAttribute("data-sound");
 
-	pad.classList.add('playing');
-	audio.currentTime = 0;
-	audio.play(); 
+		var audioElement = document.querySelector("audio[src='" + soundURL + "']");
+		console.log(audioElement);
+
+
+
+
+		audioElement.currentTime = 0;
+		audioElement.play();
 
 }
+
+window.addEventListener("keydown", onKeyPress);
+
+function onKeyPress(e) {
+	var audioSrc = e.code;
+
+	console.log(audioSrc);
+}
+
+// MDN code keyQ
+
+
+
+
+
+
 
 pads = Array.from(document.querySelectorAll('#pads'));
 
