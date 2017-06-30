31068bbcf75f6da0e838da986fdf2f0f7f9f9c83
RafM <rkjmosiolek@gmail.com>
2017-06-30 15:36:55 +0100

    update readme file

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,51 +1,66 @@
 // declare variables
-var pad, pads;
+// var pad, pads;
 
+// store reference to the array of the html button elements
+var pad = Array.from(document.querySelectorAll("button"));
 
-pad = document.querySelectorAll("button");
-
+// loop throgh the pad array
 for (var i = 0; i < pad.length; i++) {
+	// add mouse event to each button
 	pad[i].addEventListener("click", playSound);
 }
 
+// function playSound plays audio files on click event
+function playSound(e) {
+
+	// add CSS transition when the audio is playing
+	e.currentTarget.classList.add('playing');
+
+	// store clicked item's data-sound attribute in soundURL variable
+	var soundURL = e.currentTarget.getAttribute("data-sound");
+
+	// store reference to the audio element source matching the soundURL variable
+	var audioElement = document.querySelector("audio[src='" + soundURL + "']");
+	console.log(audioElement);
+
+	// schedule the audio playback on 0
+	audioElement.currentTime = 0;
+	// begin a playback of the media stored in audioElement variable
+	audioElement.play();
+}
+
+// trigger a function onKeyDown when the keyboard is clicked
+window.addEventListener("keydown", onKeyDown);
+
+// function onKeyDown plays audio files on keydown event
+function onKeyDown(e) {
+
+	// store a .code reference of pressed button (e) as a audioKeyCode variable
+	var audioKeyCode = e.code;
+	console.log(audioKeyCode);
+
+	// store reference to the audio element source matching the audioKeyCode variable
+	var audioSrc = document.querySelector("audio[data-key='" + audioKeyCode + "']");
+	console.log(audioSrc);
+
+	// schedule to audio playback on 0
+	audioSrc.currentTime = 0;
+	// begin a playback of the media elemement stored in audioSrc variable
+	audioSrc.play();
+
+	// add a CSS transition to pressed button
+	var audioPad = document.querySelector("button[data-key='" + audioKeyCode + "']");
+	audioPad.classList.add("playing");
+}
+
+// remove CSS animation if the audio is not playing
 function removeTransition(e) {
 	if (e.propertyName !== 'transform') return;
 	e.target.classList.remove('playing');
 }
 
-function playSound(e) {
-
-	e.currentTarget.classList.add('playing');
-	var soundURL = e.currentTarget.getAttribute("data-sound");
-
-	var audioElement = document.querySelector("audio[src='" + soundURL + "']");
-	console.log(audioElement);
-
-	audioElement.currentTime = 0;
-	audioElement.play();
-}
-
-window.addEventListener("keydown", onKeyDown);
-
-function onKeyDown(e) {
-
-	var audioKeyCode = e.code;
-	console.log(audioKeyCode);
-
-	var audioSrc = document.querySelector("audio[data-key='" + audioKeyCode + "']");
-	console.log(audioSrc);
-
-	audioSrc.currentTime = 0;
-	audioSrc.play();
-
-	var audioPad = document.querySelector("button[data-key='" + audioKeyCode + "']");
-	audioPad.classList.add("playing");
-}
-
-
-pads = Array.from(document.querySelectorAll('#pads'));
-
-pads.forEach(function(pad) {
+// trigger the CSS transition removal
+pad.forEach(function(pad) {
 	return pad.addEventListener('transitionend', removeTransition);
 });
 
