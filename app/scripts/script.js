e65986cb8209740dc8bafd99a0a999a6473f1278
RafM <rkjmosiolek@gmail.com>
2017-06-30 14:13:26 +0100

    app fully working

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,15 +1,8 @@
 // declare variables
-var audio, pad, pads, audioData;
+var pad, pads;
 
-audio = document.querySelector("audio");
+
 pad = document.querySelectorAll("button");
-audioData = document.querySelectorAll("audio[src]");
-
-var padAudio = document.querySelectorAll("button[data-sound='sounds/kick.wav']");
-
-
-// window.addEventListener("keydown", playSound);
-// pad.addEventListener("click", playSound);
 
 for (var i = 0; i < pad.length; i++) {
 	pad[i].addEventListener("click", playSound);
@@ -21,36 +14,20 @@ function removeTransition(e) {
 }
 
 function playSound(e) {
-	// var audio = document.querySelector('audio[data-key]');
-	// var pad = document.querySelector('button[data-key]');
 
+	e.currentTarget.classList.add('playing');
+	var soundURL = e.currentTarget.getAttribute("data-sound");
 
+	var audioElement = document.querySelector("audio[src='" + soundURL + "']");
+	console.log(audioElement);
 
-	// var audioData = audio.getAttribute("data-key");
-	// var padData = audio.getAttribute("data-key");
-
-	// if (!audio) return;
-
-
-		e.currentTarget.classList.add('playing');
-		var soundURL = e.currentTarget.getAttribute("data-sound");
-
-		var audioElement = document.querySelector("audio[src='" + soundURL + "']");
-		console.log(audioElement);
-
-
-
-
-		audioElement.currentTime = 0;
-		audioElement.play();
-
+	audioElement.currentTime = 0;
+	audioElement.play();
 }
 
 window.addEventListener("keydown", onKeyDown);
 
 function onKeyDown(e) {
-
-
 
 	var audioKeyCode = e.code;
 	console.log(audioKeyCode);
@@ -63,26 +40,7 @@ function onKeyDown(e) {
 
 	var audioPad = document.querySelector("button[data-key='" + audioKeyCode + "']");
 	audioPad.classList.add("playing");
-
 }
-
-// document.addEventListener('keydown', function(e) {
-//     var soundId = sound[e.which || e.keyCode];
-
-//     if (soundId) {
-//         var elem = document.getElementById(soundId);
-
-//         if ( elem.paused ) {
-//             elem.play();
-//         } else {
-//             elem.pause();
-//         }
-//     } else {
-//         console.log("key not mapped : code is", e.keyCode);
-//     }
-// });
-
-
 
 
 pads = Array.from(document.querySelectorAll('#pads'));
@@ -91,6 +49,6 @@ pads.forEach(function(pad) {
 	return pad.addEventListener('transitionend', removeTransition);
 });
 
-// window.addEventListener('keydown', playSound); 
+
 
 
